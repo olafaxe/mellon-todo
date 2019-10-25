a94bc00dcbadc816ae0e01170a6ee667ab2f6d52
Ola Faxe <olafaxe@gmail.com>
2019-10-26 01:30:00 +0200

    Updates

---
+++ precache-manifest.7feb2acf94bbd4a8ebdff5f2755a7420.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613f4b2573e2ad6e3abf2062ff5887f8",
+    "url": "/mellon-todo/index.html"
+  },
+  {
+    "revision": "af3ca42363a34ead9486",
+    "url": "/mellon-todo/static/css/main.6c441438.chunk.css"
+  },
+  {
+    "revision": "2a4bf882b23148dc051c",
+    "url": "/mellon-todo/static/js/2.bd911c5e.chunk.js"
+  },
+  {
+    "revision": "af3ca42363a34ead9486",
+    "url": "/mellon-todo/static/js/main.60eb995e.chunk.js"
+  },
+  {
+    "revision": "a67466bf95a60c6354a1",
+    "url": "/mellon-todo/static/js/runtime-main.feee9863.js"
+  }
+]);
