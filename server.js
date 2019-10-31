ca66437eda88c960e0194fd11762ea86d90c1e63
Ola Faxe <olafaxe@gmail.com>
2019-10-31 15:59:04 +0100

    cleanup

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ let db;
 
 client.connect(function(err) {
   if (err) throw err;
-  //users is the name of the collection from the db document
   db = client.db("heroku_24b4q4wg");
   // db = client.db("mellontodo");
 });
